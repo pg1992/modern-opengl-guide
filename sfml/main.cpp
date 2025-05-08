d837f72e79c06b73f3ef48d072efb52f3d28f967
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-05-08 21:14:50 +0200

    Create a window with SFML

--- sfml/main.cpp
+++ sfml/main.cpp
@@ -1,7 +1,70 @@
-#include <SFML/System.hpp>
+#include <iostream>
+
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
 
 int main()
 {
-    sf::sleep(sf::seconds(1.f));
+    sf::ContextSettings settings;
+    settings.depthBits = 24;
+    settings.stencilBits = 8;
+    settings.antiAliasingLevel = 2;  // Optional
+    // Request OpenGL version 3.2
+    settings.majorVersion = 3;
+    settings.minorVersion = 2;
+    settings.attributeFlags = sf::ContextSettings::Core;
+
+    std::cout << "Settings:" << std::endl;
+    std::cout << "  depth bits: " << settings.depthBits << std::endl;
+    std::cout << "  stencil bits: " << settings.stencilBits << std::endl;
+    std::cout << "  antialiasing level: " << settings.antiAliasingLevel << std::endl;
+    std::cout << "  version: " << settings.majorVersion << "." << settings.minorVersion << std::endl;
+
+    // create the window
+    sf::Window window(sf::VideoMode({800, 600}), "OpenGL", sf::Style::Close, sf::State::Windowed, settings);
+
+    sf::ContextSettings actualSettings = window.getSettings();
+
+    std::cout << "Actual settings:" << std::endl;
+    std::cout << "  depth bits: " << actualSettings.depthBits << std::endl;
+    std::cout << "  stencil bits: " << actualSettings.stencilBits << std::endl;
+    std::cout << "  antialiasing level: " << actualSettings.antiAliasingLevel << std::endl;
+    std::cout << "  version: " << actualSettings.majorVersion << "." << actualSettings.minorVersion << std::endl;
+
+    window.setVerticalSyncEnabled(true);
+
+    // activate the window
+    window.setActive(true);
+
+    // run the main loop
+    bool running = true;
+    while (running)
+    {
+        // handle events
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                // end the program
+                running = false;
+            }
+            else if (const auto* resized = event->getIf<sf::Event::Resized>())
+            {
+                // adjust the viewport when the window is resized
+                glViewport(0, 0, resized->size.x, resized->size.y);
+            }
+        }
+
+        // clear the buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // draw...
+
+        // end the current frame (internally swaps the front and back buffers)
+        window.display();
+    }
+
+    // release resources...
+
     return 0;
 }
