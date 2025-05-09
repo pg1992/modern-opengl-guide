0ae8e33456d534cd78f1883dd43004fdea655b01
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2025-05-09 23:22:00 +0200

    Add example with GLFW and GLEW

---
+++ glfw/01-opengl/main.cpp
@@ -0,0 +1,40 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <cstdio>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);  // Windowed
+    GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL", glfwGetPrimaryMonitor(), nullptr);  // Fullscreen
+
+    glfwMakeContextCurrent(window);
+
+    // Testing GLEW
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+
+    printf("%u\n", vertexBuffer);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    glfwTerminate();
+    return 0;
+}
